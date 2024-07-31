3d459a18f9bdb156709fff4328e993722db25c15
Dilan ÖZER <dilanozer@Dilans-MacBook-Air.local>
2024-07-31 20:07:00 +0300

    onion mimarisi altyapisi insa

---
+++ ETicaretAPI/Core/ETicaretAPI.Application/Abstractions/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using ETicaretAPI.Domain.Entities;
+
+namespace ETicaretAPI.Application.Abstractions
+{
+	public interface IProductService
+	{
+		List<Product> GetProducts();
+	}
+}
+
